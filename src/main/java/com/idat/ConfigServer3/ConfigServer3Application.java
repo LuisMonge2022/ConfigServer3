3cf0b76e9fc610d499676242ce87a33e2a215b29
LuisMonge2022 <luisyangelo2011@gmail.com>
2022-08-18 23:31:55 -0500

    ConfigServer3

---
+++ src/main/java/com/idat/ConfigServer3/ConfigServer3Application.java
@@ -0,0 +1,16 @@
+package com.idat.ConfigServer3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServer3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServer3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/idat/ConfigServer3/ConfigServer3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.ConfigServer3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServer3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
